da77db0fe5d4322b091d1b496ed6caf1b43fdf1a
dhandameghanareddy <meghanareddy2207@gmail.com>
2026-08-13 23:27:42 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/index.js
+++ frontend/index.js
@@ -2,27 +2,41 @@
 
  var users = [
      {
-        "name": "John Doe",
-        "gender": "Male",
-        "image": "john.png"
+        "name": "meghana",
+        "image": "meghana.jpeg"
      },
      {
-        "name": "Jane Doe",
-        "gender": "Female",
-        "image": "jane.png"
+        "name": "showrya",
+        "image": "showrya.jpeg"
+     },
+     {
+         "name": "chandrika",
+         "image": "chandrika.jpeg"
      }
  ]
  var curId = 0;
 
  function toggleUser(){
-    curId=(curId+1)%2;
+    curId=(curId+1)%3;
 
     var userName = document.getElementById("user-name");
-    var userGender = document.getElementById("user-gender");
+    //var userGender = document.getElementById("user-gender");
     var userImage = document.getElementById("user-image");   
 
     userName.innerHTML = users[curId].name;
-    userGender.innerHTML = users[curId].gender;
+    //userGender.innerHTML = users[curId].gender;
     userImage.src = users[curId].image;
  }
+/*let h2=document.querySelector("h2");
+let img=document.querySelector("img");
+let btn=document.querySelector("button");
+btn.addEventListener("click",()=>{
+   h2.innerText="showrya";
+   img.src="showrya.jpeg";
+});
+btn.addEventListener("mouseover",()=>{
+   h2.innerText="chandrika";
+   img.src="chandrika.jpeg";
+});*/
+
 

43361b363370bc9f3da889f870b410bf6558d345
dhandameghanareddy <meghanareddy2207@gmail.com>
2026-08-14 09:56:36 +0530

    Update user information and enhance toggle functionality in HTML and JavaScript

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,7 @@
 //alert("welcome to web bootcamp");
 
  var users = [
-     {
+   /* {
         "name": "meghana",
         "image": "meghana.jpeg"
      },
@@ -12,19 +12,29 @@
      {
          "name": "chandrika",
          "image": "chandrika.jpeg"
-     }
+     }*/
+    {
+      "name": "John Doe",
+      "image": "john.png",
+      "gender": "Male"
+    },
+    {
+      "name": "Jane Doe",
+      "image": "jane.png",
+      "gender": "Female"
+    }
  ]
  var curId = 0;
 
  function toggleUser(){
-    curId=(curId+1)%3;
+    curId=(curId+1)%2;
 
     var userName = document.getElementById("user-name");
-    //var userGender = document.getElementById("user-gender");
+    var userGender = document.getElementById("user-gender");
     var userImage = document.getElementById("user-image");   
 
     userName.innerHTML = users[curId].name;
-    //userGender.innerHTML = users[curId].gender;
+    userGender.innerHTML = users[curId].gender;
     userImage.src = users[curId].image;
  }
 /*let h2=document.querySelector("h2");
